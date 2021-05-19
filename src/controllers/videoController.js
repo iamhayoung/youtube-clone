718da2644140390fd5ab73a1f81b28c31c80f58a
Hayoung Gam <hahhyoung@gmail.com>
2021-05-19 17:57:35 +0900

    Using Pug Template Variables

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,8 @@
-export const trending = (req, res) => res.render("home"); // views/home.pug
+// template(base.pug)을 렌더링하는 것은 Controller임.
+// render의 첫번째 인자는 렌더링할 파일의 이름
+// 두번째 인자는 pug template(base.pug)에 보낼 변수임.
+// 각페이지의 views/base.pug의 pageTitle이라는 값에 들어갈 값을 넣어줌
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", potato: "potatoooo" }); // views/home.pug
 export const see = (req, res) => res.render("watch"); // views/watch.pug
 export const edit = (req, res) => res.render("edit"); // views/edit.pug
 

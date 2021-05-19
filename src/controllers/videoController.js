92a4fabe19eb4284ae38382d22dcd7bde8298780
Hayoung Gam <hahhyoung@gmail.com>
2021-05-19 19:48:35 +0900

    Using Pug Array Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,11 @@
-const fakeUser = {
-  username: "Hayoung",
-  loggedIn: false
-}
-
 // template(base.pug)을 렌더링하는 것은 Controller임.
 // render의 첫번째 인자는 렌더링할 파일의 이름
 // 두번째 인자는 pug template(base.pug)에 보낼 변수임.
 // 각페이지의 views/base.pug의 pageTitle이라는 값에 들어갈 값을 넣어줌
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser }); // views/home.pug
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
+}; // views/home.pug
 export const see = (req, res) => res.render("watch"); // views/watch.pug
 export const edit = (req, res) => res.render("edit"); // views/edit.pug
 

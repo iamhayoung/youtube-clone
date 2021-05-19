68445d36a2ee200f2862913cc60076411aa9fd39
Hayoung Gam <hahhyoung@gmail.com>
2021-05-19 17:21:38 +0900

    Include footer (using Pug)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,6 @@
 export const trending = (req, res) => res.render("home"); // views/home.pug
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-
-export const edit = (req, res) => {
-  return res.send("Edit Video");
-};
+export const see = (req, res) => res.render("watch"); // views/watch.pug
+export const edit = (req, res) => res.render("edit"); // views/edit.pug
 
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");

0d0fd86fe98326bc5376899234eeb4790795f6d7
Hayoung Gam <hahhyoung@gmail.com>
2021-05-23 18:05:26 +0900

    Create Mongoose hashtag middleware

--- src/models/Video.js
+++ src/models/Video.js
@@ -12,6 +12,16 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+// mongoose에서는 document에 무슨 일이 생기기 전이나 후에 middleware를 적용할 수 있다
+// 예를 들면 save하기 전,후로 middleware를 적용하거나 function을 실행할수 있다
+// middleware는 무조건 model이 생성되기 전에 만들어야한다!
+videoSchema.pre("save", async function () {
+  // this는 우리가 저장하고자 하는 document(input에서 입력된 값)를 가리킴
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map(word => word.startsWith("#") ? word : `#${word}`);
+});
+
 // model: 데이터가 어떻게 생겼는지 데이터베이스에게 설명해줌. title이 string인지, 배열인지, 숫자인지...
 // model의 첫번째 인자: model의 이름
 // 두번쨰 인자: 데이터의 형태인 schema

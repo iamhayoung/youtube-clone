c07585b877235cb4caf83de0128cc708e98110a9
Hayoung Gam <hahhyoung@gmail.com>
2021-10-02 00:57:52 +0900

    Add: Create Signup and hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,18 @@
+import User from '../models/User';
+
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
-export const postJoin = (req, res) => {
-  console.log(req.body);
+export const postJoin = async (req, res) => {
+  const { email, username, password, name, location } = req.body;
+
+  try {
+    await User.create({ email, username, password, name, location });
+    return res.redirect('/login');
+  } catch (error) {
+    return res.render('404', {
+      pageTitle: 'Join',
+      errorMessage: error._message,
+    });
+  }
 };
 export const edit = (req, res) => res.send('Edit');
 export const remove = (req, res) => res.send('Remove');
--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre('save', async function () {
+  // pre 미들웨어를 이용해서 저장하기 전에 비밀번호를 해싱 후 저장
+  // saltRounds: 몇번 더 해싱할지.
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model('User', userSchema);

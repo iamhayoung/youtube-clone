4dbf8ebce2fedd97a21a4e6b2d79adb426ba4e51
Hayoung Gam <hahhyoung@gmail.com>
2021-10-02 01:16:25 +0900

    Add: Signup email, username, password validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,16 +2,43 @@ import User from '../models/User';
 
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
 export const postJoin = async (req, res) => {
-  const { email, username, password, name, location } = req.body;
+  const { email, username, password, password2, name, location } = req.body;
+  const pageTitle = 'Join';
+
+  if (password !== password2) {
+    return res.render('join', {
+      pageTitle,
+      errorMessage: 'Password confirmation does not match.',
+    });
+  }
+
+  // 몽고 $or operator: 각 조건이 true일 때 실행되게 만들 수 있음
+  // 배열안에 조건을 적음.
+  // 첫번째 조건: req.body.username과 일치하는 User를 찾음
+  // 첫번째 조건: req.body.email 일치하는 User를 찾음
+  const exists = await User.exists({
+    $or: [{ username }, { email }],
+  });
+  if (exists) {
+    return res.render('join', {
+      pageTitle,
+      errorMessage: 'This username/email is already taken.',
+    });
+  }
+
+  // const emailExists = await User.exists({ username });
+  // if (emailExists) {
+  //   return res.render('join', {
+  //     pageTitle,
+  //     errorMessage: 'This email is already taken.',
+  //   });
+  // }
 
   try {
     await User.create({ email, username, password, name, location });
     return res.redirect('/login');
   } catch (error) {
-    return res.render('404', {
-      pageTitle: 'Join',
-      errorMessage: error._message,
-    });
+    console.log(error);
   }
 };
 export const edit = (req, res) => res.send('Edit');

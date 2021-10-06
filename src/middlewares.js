cdffd423cf3620d3c5e89b641ebee849281b7c86
Hayoung Gam <hahhyoung@gmail.com>
2021-10-06 23:17:21 +0900

    Create protectorMiddleware (미로그인시 로그인 페이지로 리다이렉트)

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,3 +9,11 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedInUser = req.session.user;
   next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect('/login');
+  }
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,12 +6,13 @@ import {
   remove,
   see,
 } from '../controllers/userController';
+import { protectorMiddleware } from '../middlewares';
 
 const userRouter = express.Router();
 
 // userRouter의 안이기 때문에 /user/edit이 아닌 /edit만 써도됨. /users를 따로 써줄 필요없음
 userRouter.get('/logout', logout);
-userRouter.route('/edit').get(getEdit).post(postEdit);
+userRouter.route('/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get('/remove', remove);
 userRouter.get('/:id', see);
 
